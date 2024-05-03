35622d199f8960aac2e78a9e0d2d4fdb7767b872
Sebastian Klemm <68734496+hey-sebi@users.noreply.github.com>
2024-05-03 19:17:13 +0200

    Readme and some bash aliases

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # dotfiles
 Contains configuration and setup files for my personal use. Use what you want, but use it at your personal risk.
+
+## Directory structure
+
+This will probably grow over time..
+
+```
+├── shell/       # For continuous integration files
+├── /            #
+├── /            #
+├── /            #
+├── /            #
+├── /            #
+└── /            #
+```
---
+++ shell/bash-aliases.sh
@@ -0,0 +1,24 @@
+# ----------------------------------------------------------------
+#  GIT & Co
+# ----------------------------------------------------------------
+
+# Commits with a timestamp of 12 hours in the future
+alias future-commit='git commit --date "$(date -d +12hours)"'
+
+
+# ----------------------------------------------------------------
+#  Editors
+# ----------------------------------------------------------------
+
+# Doom emacs' 'doom' executable
+alias doom='~/.config/emacs/bin/doom'
+# NeoVim from appimage
+alias nvim='~/nvim.appimage'
+
+
+# ----------------------------------------------------------------
+#  Mixed
+# ----------------------------------------------------------------
+
+# Use python3 per default
+alias python='python3'
