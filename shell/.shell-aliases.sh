e19b3affaee84ce3d1d7934ef007d6694fe54816
Sebastian Klemm <68734496+hey-sebi@users.noreply.github.com>
2025-11-21 20:33:27 +0100

    Add tm alias (new tmux session based on dir)

--- shell/.shell-aliases.sh
+++ shell/.shell-aliases.sh
@@ -14,3 +14,34 @@ alias ll='ls -al'
 
 # Lazygit shorthand
 alias lg='lazygit'
+
+# Creates a new tmux session in a dir that can be fuzzy-selected
+#
+# Requires zoxide and fzf to be installed
+new_tmux() {
+  session_dir=$(zoxide query --list | fzf)
+  session_name=$(basename "$session_dir")
+
+  if tmux has-session -t $session_name 2>/dev/null; then
+    if [ -n "$TMUX" ]; then
+      tmux switch-client -t "$session_name"
+    else
+      tmux attach -t "$session_name"
+    fi
+    notification="tmux attached to $session_name"
+  else
+    if [ -n "$TMUX" ]; then
+      tmux new-session -d -c "$session_dir" -s "$session_name" && tmux switch-client -t "$session_name"
+      notification="new tmux session INSIDE TMUX: $session_name"
+    else
+      tmux new-session -c "$session_dir" -s "$session_name"
+      notification="new tmux session: $session_name"
+    fi
+  fi
+
+  if [-s "$session_name" ]; then
+    notify-send "$notification"
+  fi
+}
+
+alias tm=new_tmux
